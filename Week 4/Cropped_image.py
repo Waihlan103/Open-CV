25fb16865d32a1dc803521e67e51c05b3af7ac85
Wai Hlan Kaung <70847243+Waihlan103@users.noreply.github.com>
2021-05-13 11:48:17 +0630

    Update Cropped_image.py

--- Week 4/Cropped_image.py
+++ Week 4/Cropped_image.py
@@ -1,37 +1,38 @@
 import cv2
 from matplotlib import pyplot as plt
+import numpy as np
 
-original_img = cv2.imread('coins2.jpg')
-imgray = cv2.cvtColor( original_img, cv2.COLOR_BGR2GRAY)
+image = cv2.imread("coin.jpeg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-blurred_image = cv2.GaussianBlur( imgray, (11,11), 0)
-edge_detection = cv2.Canny( blurred_image, 30, 150)
+blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+edged = cv2.Canny(blurred, 30, 150)
 
-contours, _ = cv2.findContours( edge_detection.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-contour_image = original_img.copy()
-cv2.drawContours(contour_image, contours, -1, (0,255,0), 3)
+contours, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-cropped = []
+contour_img = image.copy()
+cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 2)
+cropped_image = []
 
 for (item, count) in enumerate(contours):
     (x, y, w, h) = cv2.boundingRect(count)
 
-    cropped_coin = original_img[y: y + h, x: x + w]
-    cropped.append(cropped_coin)
+    print("Coin #{}".format(item+1))
+    coin = image[y:y + h, x:x + w]
+    cropped_image.append(coin)
 
-titles = ['Original_image', 'Blurred Image', 'Canny Image', 'Contoured Image','Cropped Image']
-images = [original_img, blurred_image, edge_detection, contour_image, cropped_coin]
-
+titles = ['Original Image','Blurred Image','Canny edged Image', 'Contoured Image']
+images = [image, blurred, edged, contour_img]
 
 for i in range(4):
-    plt.subplot(2, 2, i+1), plt.imshow(original_img[i], 'gray')
+    plt.subplot(2, 2, i+1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 
 plt.figure()
 
-for i in range(5):
-    plt.subplot(2, 3, i+1), plt.imshow(cropped[i], 'gray')
+for i in range(8):
+    plt.subplot(2, 4, i + 1), plt.imshow(cropped_image[i], 'gray')
     plt.xticks([]), plt.yticks([])
 
 plt.show()
